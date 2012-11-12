277023e10b89a70fde7286487922eaf925ef1213
nicolas <renard760@gmail.com>
2012-11-12 11:23:48 +0100

    refs #242, fixes #242

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,8 +34,8 @@ ProjestimateMaquette::Application.configure do
     :address              => "smtp.gmail.com",
     :port                 => 587,
     :domain               => "gmail.com",
-    :user_name            => "renard760",
-    :password             => "spirulaspirulaspirula",
+    :user_name            => "",
+    :password             => "",
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
