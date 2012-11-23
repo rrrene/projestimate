d31b6b168e4df6fec84e1a00fd0d312c79d536df
nicolas <renard760@gmail.com>
2012-11-23 15:41:35 +0100

    refs #267

--- app/helpers/searches_helper.rb
+++ app/helpers/searches_helper.rb
@@ -19,7 +19,15 @@
 ########################################################################
 
 module SearchesHelper
-  def display_result(res, params)
-    link_to raw("#{ res.to_s.gsub(/(#{params})/i, '<strong>\1</strong>')}") , "/#{String::keep_clean_space(res.class.to_s.underscore.pluralize)}/#{res.id}/edit" , :class => "search_result"
+  def display_link(res, params)
+    link_to(raw("#{ res.to_s.gsub(/(#{params})/i, '<strong>\1</strong>')}") , "/#{String::keep_clean_space(res.class.to_s.underscore.pluralize)}/#{res.id}/edit" , :class => "search_result")
+  end
+
+  def display_description(res, params)
+    res.description
+  end
+
+  def display_update(res, params)
+    "Last update #{res.updated_at.strftime("%d %m %Y")}"
   end
 end
