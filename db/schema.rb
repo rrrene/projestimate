6cc40aa6df4782e510ba527390ef9aafb35a779a
nicolas <renard760@gmail.com>
2012-11-30 14:15:05 +0100

    remove active_directory

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121112123539) do
+ActiveRecord::Schema.define(:version => 20121123144434) do
 
   create_table "acquisition_categories", :force => true do |t|
     t.string   "name"
@@ -88,8 +88,7 @@ ActiveRecord::Schema.define(:version => 20121112123539) do
     t.integer  "port"
     t.string   "base_dn"
     t.string   "user_name_attribute"
-    t.string   "certificate"
-    t.string   "scope"
+    t.boolean  "certificate"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
