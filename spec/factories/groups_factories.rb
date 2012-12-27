02a2daa0c15a4ffa044c80d4bb6051744e92f3b0
spirula-sga <salimata.gaye@spirula.fr>
2012-12-27 15:54:19 +0100

    Adding group_factories: refs #303

---
+++ spec/factories/groups_factories.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+
+  factory :group do
+    sequence(:name) {|n| "group#{n}"}
+    sequence(:description) {|n| "Group number #{n}"}
+    uuid
+    association :record_status, :factory => :proposed_status, strategy: :build
+  end
+
+end
