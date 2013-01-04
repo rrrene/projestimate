6d0b1c7736e468713d12ee9a8385cc20fe1e3e7f
nicolas <renard760@gmail.com>
2013-01-04 10:46:04 +0100

    refs #247

--- app/models/external_master_database.rb
+++ app/models/external_master_database.rb
@@ -13,48 +13,64 @@ module ExternalMasterDatabase
 
   class ExternalAcquisitionCategory < ActiveRecord::Base
     establish_connection HOST
+    has_one    :child,  :class_name => "ExternalAcquisitionCategory", :inverse_of => :parent, :foreign_key => "parent_id"
+    belongs_to :parent, :class_name => "ExternalAcquisitionCategory", :inverse_of => :child,  :foreign_key => "parent_id"
     self.table_name = "acquisition_categories"
     scope :defined, lambda {|cs| where("record_status_id = ?", cs) }
   end
 
   class ExternalActivityCategory < ActiveRecord::Base
     establish_connection HOST
+    has_one    :child,  :class_name => "ExternalActivityCategory", :inverse_of => :parent, :foreign_key => "parent_id"
+    belongs_to :parent, :class_name => "ExternalActivityCategory", :inverse_of => :child,  :foreign_key => "parent_id"
     self.table_name = "activity_categories"
     scope :defined, lambda {|cs| where("record_status_id = ?", cs) }
   end
 
   class ExternalAttribute < ActiveRecord::Base
     establish_connection HOST
+    has_one    :child,  :class_name => "ExternalAttribute", :inverse_of => :parent, :foreign_key => "parent_id"
+    belongs_to :parent, :class_name => "ExternalAttribute", :inverse_of => :child,  :foreign_key => "parent_id"
     self.table_name = "attributes"
     scope :defined, lambda {|cs| where("record_status_id = ?", cs) }
   end
 
   class ExternalAttributeModule < ActiveRecord::Base
     establish_connection HOST
+    has_one    :child,  :class_name => "ExternalAttributeModule", :inverse_of => :parent, :foreign_key => "parent_id"
+    belongs_to :parent, :class_name => "ExternalAttributeModule", :inverse_of => :child,  :foreign_key => "parent_id"
     self.table_name = "attributes_modules"
     scope :defined, lambda {|cs| where("record_status_id = ?", cs) }
   end
 
   class ExternalCurrency < ActiveRecord::Base
     establish_connection HOST
+    has_one    :child,  :class_name => "ExternalCurrency", :inverse_of => :parent, :foreign_key => "parent_id"
+    belongs_to :parent, :class_name => "ExternalCurrency", :inverse_of => :child,  :foreign_key => "parent_id"
     self.table_name = "currencies"
     scope :defined, lambda {|cs| where("record_status_id = ?", cs) }
   end
 
   class ExternalEventType < ActiveRecord::Base
     establish_connection HOST
+    has_one    :child,  :class_name => "ExternalEventType", :inverse_of => :parent, :foreign_key => "parent_id"
+    belongs_to :parent, :class_name => "ExternalEventType", :inverse_of => :child,  :foreign_key => "parent_id"
     self.table_name = "event_types"
     scope :defined, lambda {|cs| where("record_status_id = ?", cs) }
   end
 
   class ExternalLaborCategory < ActiveRecord::Base
     establish_connection HOST
+    has_one    :child,  :class_name => "ExternalLaborCategory", :inverse_of => :parent, :foreign_key => "parent_id"
+    belongs_to :parent, :class_name => "ExternalLaborCategory", :inverse_of => :child,  :foreign_key => "parent_id"
     self.table_name = "labor_categories"
     scope :defined, lambda {|cs| where("record_status_id = ?", cs) }
   end
 
   class ExternalLanguage < ActiveRecord::Base
     establish_connection HOST
+    has_one    :child,  :class_name => "ExternalLanguage", :inverse_of => :parent, :foreign_key => "parent_id"
+    belongs_to :parent, :class_name => "ExternalLanguage", :inverse_of => :child,  :foreign_key => "parent_id"
     self.table_name = "languages"
     scope :defined, lambda {|cs| where("record_status_id = ?", cs) }
   end
@@ -69,24 +85,32 @@ module ExternalMasterDatabase
 
   class ExternalPeicon < ActiveRecord::Base
     establish_connection HOST
+    has_one    :child,  :class_name => "ExternalPeicon", :inverse_of => :parent, :foreign_key => "parent_id"
+    belongs_to :parent, :class_name => "ExternalPeicon", :inverse_of => :child,  :foreign_key => "parent_id"
     self.table_name = "peicons"
     scope :defined, lambda {|cs| where("record_status_id = ?", cs) }
   end
 
   class ExternalPemodule < ActiveRecord::Base
     establish_connection HOST
+    has_one    :child,  :class_name => "ExternalPemodule", :inverse_of => :parent, :foreign_key => "parent_id"
+    belongs_to :parent, :class_name => "ExternalPemodule", :inverse_of => :child,  :foreign_key => "parent_id"
     self.table_name = "pemodules"
     scope :defined, lambda {|cs| where("record_status_id = ?", cs) }
   end
 
   class ExternalPlatformCategory < ActiveRecord::Base
     establish_connection HOST
+    has_one    :child,  :class_name => "ExternalPlatformCategory", :inverse_of => :parent, :foreign_key => "parent_id"
+    belongs_to :parent, :class_name => "ExternalPlatformCategory", :inverse_of => :child,  :foreign_key => "parent_id"
     self.table_name = "platform_categories"
     scope :defined, lambda {|cs| where("record_status_id = ?", cs) }
   end
 
   class ExternalProjectArea < ActiveRecord::Base
     establish_connection HOST
+    has_one    :child,  :class_name => "ExternalProjectArea", :inverse_of => :parent, :foreign_key => "parent_id"
+    belongs_to :parent, :class_name => "ExternalProjectArea", :inverse_of => :child,  :foreign_key => "parent_id"
     self.table_name = "project_areas"
     scope :defined, lambda {|cs| where("record_status_id = ?", cs) }
   end
@@ -94,35 +118,47 @@ module ExternalMasterDatabase
   class ExternalProjectCategory < ActiveRecord::Base
     scope :defined, lambda {|cs| where("record_status_id = ?", cs) }
     establish_connection HOST
+    has_one    :child,  :class_name => "ExternalProjectCategory", :inverse_of => :parent, :foreign_key => "parent_id"
+    belongs_to :parent, :class_name => "ExternalProjectCategory", :inverse_of => :child,  :foreign_key => "parent_id"
     self.table_name = "project_categories"
   end
 
   class ExternalProjectSecurityLevel < ActiveRecord::Base
     establish_connection HOST
+    has_one    :child,  :class_name => "ExternalProjectSecurityLevel", :inverse_of => :parent, :foreign_key => "parent_id"
+    belongs_to :parent, :class_name => "ExternalProjectSecurityLevel", :inverse_of => :child,  :foreign_key => "parent_id"
     self.table_name = "project_security_levels"
     scope :defined, lambda {|cs| where("record_status_id = ?", cs) }
   end
 
   class ExternalRecordStatus < ActiveRecord::Base
     establish_connection HOST
+    has_one    :child,  :class_name => "ExternalRecordStatus", :inverse_of => :parent, :foreign_key => "parent_id"
+    belongs_to :parent, :class_name => "ExternalRecordStatus", :inverse_of => :child,  :foreign_key => "parent_id"
     self.table_name = "record_status"
     scope :defined, lambda {|cs| where("record_status_id = ?", cs) }
   end
 
   class ExternalWorkElementType < ActiveRecord::Base
     establish_connection HOST
+    has_one    :child,  :class_name => "ExternalWorkElementType", :inverse_of => :parent, :foreign_key => "parent_id"
+    belongs_to :parent, :class_name => "ExternalWorkElementType", :inverse_of => :child,  :foreign_key => "parent_id"
     self.table_name = "work_element_types"
     scope :defined, lambda {|cs| where("record_status_id = ?", cs) }
   end
 
   class ExternalAdminSetting < ActiveRecord::Base
     establish_connection HOST
+    has_one    :child,  :class_name => "ExternalAdminSetting", :inverse_of => :parent, :foreign_key => "parent_id"
+    belongs_to :parent, :class_name => "ExternalAdminSetting", :inverse_of => :child,  :foreign_key => "parent_id"
     self.table_name = "admin_settings"
     scope :defined, lambda {|cs| where("record_status_id = ?", cs) }
   end
 
   class ExternalAuthMethod < ActiveRecord::Base
     establish_connection HOST
+    has_one    :child,  :class_name => "ExternalAuthMethod", :inverse_of => :parent, :foreign_key => "parent_id"
+    belongs_to :parent, :class_name => "ExternalAuthMethod", :inverse_of => :child,  :foreign_key => "parent_id"
     self.table_name = "auth_methods"
     scope :defined, lambda {|cs| where("record_status_id = ?", cs) }
   end
@@ -137,12 +173,16 @@ module ExternalMasterDatabase
 
   class ExternalPermission < ActiveRecord::Base
     establish_connection HOST
+    has_one    :child,  :class_name => "ExternalPermission", :inverse_of => :parent, :foreign_key => "parent_id"
+    belongs_to :parent, :class_name => "ExternalPermission", :inverse_of => :child,  :foreign_key => "parent_id"
     self.table_name = "permissions"
     scope :defined, lambda {|cs| where("record_status_id = ?", cs) }
   end
 
   class ExternalRecordStatus < ActiveRecord::Base
     establish_connection HOST
+    has_one    :child,  :class_name => "ExternalRecordStatus", :inverse_of => :parent, :foreign_key => "parent_id"
+    belongs_to :parent, :class_name => "ExternalRecordStatus", :inverse_of => :child,  :foreign_key => "parent_id"
     self.table_name = "record_statuses"
     scope :defined, lambda {|cs| where("record_status_id = ?", cs) }
   end
