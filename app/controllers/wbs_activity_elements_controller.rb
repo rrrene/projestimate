64df5274d8c826651e4bf2d99412ebf68b1eed14
nicolas <renard760@gmail.com>
2013-02-01 12:57:41 +0100

    refs #333

--- app/controllers/wbs_activity_elements_controller.rb
+++ app/controllers/wbs_activity_elements_controller.rb
@@ -33,7 +33,11 @@ class WbsActivityElementsController < ApplicationController
 
     if params[:activity_id]
       @wbs_activity = WbsActivity.find(params[:activity_id])
-      @potential_parents = @wbs_activity.wbs_activity_elements
+      if @wbs_activity_element.ancestry.nil?
+        @potential_parents = []
+      else
+        @potential_parents = @wbs_activity.wbs_activity_elements
+      end
     end
 
     @selected_parent = @wbs_activity_element.parent
@@ -51,7 +55,7 @@ class WbsActivityElementsController < ApplicationController
         @wbs_activity_element.record_status = @local_status
       else
         flash[:error] = "Master record can not be deleted, it is required for the proper functioning of the application"
-        redirect_to wbs_activity_elements_url
+        redirect_to wbs_activity_element_path(@wbs_activity.id)
       end
     end
   end
@@ -84,14 +88,13 @@ class WbsActivityElementsController < ApplicationController
 
     @wbs_activity ||= WbsActivity.find_by_id(params[:activity_id])
     @potential_parents = @wbs_activity.wbs_activity_elements if @wbs_activity
-
     @selected_parent = current_wbs_activity_element
 
     if current_wbs_activity_element.is_defined?
       @wbs_activity_element = current_wbs_activity_element.amoeba_dup
       @wbs_activity_element.owner_id = current_user.id
     else
-      @wbs_activity_element = current_wbs_activity_element.parent
+      @wbs_activity_element = current_wbs_activity_element
     end
 
     unless is_master_instance?
@@ -104,7 +107,7 @@ class WbsActivityElementsController < ApplicationController
       @wbs_activity = WbsActivity.find(params[:wbs_activity_element][:wbs_activity_id])
     end
 
-    if current_wbs_activity_element.update_attributes(params[:wbs_activity_element])
+    if @wbs_activity_element.update_attributes(params[:wbs_activity_element])
       redirect_to wbs_activity_element_path(@wbs_activity.id), notice: 'Wbs activity element was successfully updated.'
     else
       render action: "edit"
