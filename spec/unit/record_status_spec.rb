36a25a28cd76d5116e5c061ef44dcb012d63f9e1
sabrina <sebbar.sabrina@gmail.com>
2013-02-04 14:02:03 +0100

    Commit Tests Controllers  refs #321

---
+++ spec/unit/record_status_spec.rb
@@ -0,0 +1,21 @@
+require "spec_helper"
+describe RecordStatus do
+  #before :each do
+  #  @RecordStatus=  FactoryGirl.create(:proposed_status)
+  #end
+  #
+  #it 'should return wbs_activity name' do
+  #  @RecordStatus.to_s().should eql(:proposed_status.name)
+  #end
+  #it "should be not valid" do
+  #  @RecordStatus.name= 1
+  #  @RecordStatus.wbs_activity_name.should_not be_instance_of(String)
+  #end
+  #
+  #it "should be valid" do
+  #  @RecordStatus.wbs_activity_name.should be_an_instance_of(String)
+  #end
+
+
+
+end
