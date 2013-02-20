e52fdf313ba3f733cf1d33e202db776b6cce45d3
nicolas <renard760@gmail.com>
2013-02-20 16:21:11 +0100

    test correction

--- spec/controllers/reference_values_controller_spec.rb
+++ spec/controllers/reference_values_controller_spec.rb
@@ -2,25 +2,4 @@ require 'spec_helper'
 
 describe ReferenceValuesController do
 
-  describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'edit'" do
-    it "returns http success" do
-      get 'edit'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'new'" do
-    it "returns http success" do
-      get 'new'
-      response.should be_success
-    end
-  end
-
 end
