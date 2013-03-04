c107944f17f912eace35055b9c17641c99d86c93
nicolas <renard760@gmail.com>
2013-03-04 11:53:46 +0100

    refs #393

--- app/controllers/wbs_activity_ratio_elements_controller.rb
+++ app/controllers/wbs_activity_ratio_elements_controller.rb
@@ -20,15 +20,15 @@ class WbsActivityRatioElementsController < ApplicationController
       w = WbsActivityRatioElement.find(i.first)
       if w.wbs_activity_ratio.is_All_Activity_Elements?
         if i.last.blank?
-           flash[:custom] = "Please insert ratio value"
+           flash.now[:custom] = "Please insert ratio value"
         elsif i.last.to_i <=0 or i.last.to_i>100
-           flash[:custom] = "Please, enter value between 0 and 100"
+           flash.now[:custom] = "Please, enter value between 0 and 100"
         else
           #
         end
       else
         if i.last.to_i <= 0 or i.last.to_i>100
-          flash[:custom] = "Please, enter value between 0 and 100"
+          flash.now[:custom] = "Please, enter value between 0 and 100"
         end
       end
       w.ratio_value = i.last
@@ -38,14 +38,6 @@ class WbsActivityRatioElementsController < ApplicationController
 
     #Select ratio and elements
     wbs_activity_ratio = WbsActivityRatio.find(params[:wbs_activity_ratio_id])
-    ratio_elements = wbs_activity_ratio.wbs_activity_ratio_elements
-
-    #keep current ratio
-    @selected_ratio = wbs_activity_ratio
-    @wbs_activity_ratio_elements = wbs_activity_ratio.wbs_activity_ratio_elements
-
-    #sum total ratio_value
-    @total = ratio_elements.reject{|i| i.ratio_value.nil? or i.ratio_value.blank? }.compact.sum(&:ratio_value)
 
     #set ratio reference (all to false then one to true)
     wbs_activity_ratio.wbs_activity_ratio_elements.each do |wbs_activity_ratio_element|
@@ -59,21 +51,30 @@ class WbsActivityRatioElementsController < ApplicationController
           new_ref.update_attribute("ratio_reference_element", true)
         end
       else
-        flash[:error] = "Please, select a reference element."
+        flash.now[:error] = "Please, select a reference element."
       end
     else
       if params[:ratio_reference_element]
         new_ref = WbsActivityRatioElement.find_by_id_and_wbs_activity_ratio_id(params[:ratio_reference_element], params[:wbs_activity_ratio_id])
         new_ref.update_attribute("ratio_reference_element", true)
       else
-        flash[:error] = "Please, select a reference element."
+        flash.now[:error] = "Please, select a reference element."
       end
     end
+
+    #keep current ratio
+    @selected_ratio = wbs_activity_ratio
+    @wbs_activity_ratio_elements = wbs_activity_ratio.wbs_activity_ratio_elements.all
+    #@wbs_activity_ratio_elements = WbsActivityRatioElement.where(:wbs_activity_ratio_id => wbs_activity_ratio.id).all
+
+    #sum total ratio_value
+    @total = @wbs_activity_ratio_elements.reject{|i| i.ratio_value.nil? or i.ratio_value.blank? }.compact.sum(&:ratio_value)
+
     #we test total
     if @total != 100
-      flash[:warning] = "Warning - Ratios successfully saved, but sum is different of 100%"
+      flash.now[:warning] = "Warning - Ratios successfully saved, but sum is different of 100%"
     else
-      flash[:notice] = "Ratios successfully saved"
+      flash.now[:notice] = "Ratios successfully saved"
     end
   end
 
