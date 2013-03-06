5a92a0786cb69137ad67c8cec2e45e7a95a334fe
nicolas <renard760@gmail.com>
2013-03-06 17:14:45 +0100

    refs #363

--- app/models/home.rb
+++ app/models/home.rb
@@ -269,14 +269,7 @@ class Home < ActiveRecord::Base
             act = WbsActivity.find_by_uuid(ext_act.uuid)
             ActiveRecord::Base.connection.execute("UPDATE wbs_activity_ratios SET wbs_activity_id = #{act.id} WHERE uuid = '#{ext_ratio.uuid}'")
           end
-        end
 
-        #Associate activity ratio elements to activity
-        ext_ratio_elements.each do |ext_ratio_element|
-          if ext_ratio.id == ext_ratio_element.wbs_activity_ratio_id and ext_ratio.record_status_id == ext_defined_rs_id
-            activity_ratio = WbsActivityRatio.find_by_uuid(ext_ratio_element.wbs_activity_ratio.uuid)
-            ActiveRecord::Base.connection.execute("UPDATE wbs_activity_ratio_elements SET wbs_activity_ratio_id = #{activity_ratio.id} WHERE uuid = '#{ext_ratio_element.uuid}'")
-          end
         end
 
       end
