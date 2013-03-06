6cbf1a81a81784ba92128a9f2e4bc1649a05cfff
spirula-sga <salimata.gaye@spirula.fr>
2013-03-06 10:04:19 +0100

    Embedded test (phase 5) : refs #387

--- spec/factories/wbs_activity_ratios_factories.rb
+++ spec/factories/wbs_activity_ratios_factories.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :wbs_activity_ratio do
+    uuid
     sequence(:name){|n| "Ratio_#{n}"}
     description "TBD"
     association :record_status, :factory => :local_status, strategy: :build
