eb341eb8841cf7911e72aa943e51c8068053ab0e
spirula-sga <salimata.gaye@spirula.fr>
2013-03-08 10:59:03 +0100

    Embedded test (phase 5) : refs #387

--- spec/unit/wbs_activity_element_spec.rb
+++ spec/unit/wbs_activity_element_spec.rb
@@ -3,18 +3,16 @@ require "spec_helper"
 describe WbsActivityElement do
 
   before :each do
-    @wbs_activity=  FactoryGirl.create(:wbs_activity)
     @wbs_activity_element = FactoryGirl.create(:wbs_activity_element)
     @wbs_activity_element2 = FactoryGirl.create(:wbs_activity_element, :name=>1)
   end
 
   it 'should return wbs_activity name' do
-    @wbs_activity_element.wbs_activity = @wbs_activity
-    @wbs_activity_element.wbs_activity_name.should eql(@wbs_activity.name)
+    @wbs_activity_element.wbs_activity_name.should eql(@wbs_activity_element.wbs_activity.name)
   end
 
-  it "should be not valid" do
-    @wbs_activity_element2.wbs_activity_name.should_not be_instance_of(String)
+  it "should be not valid without wbs-activity" do
+    @wbs_activity_element2.wbs_activity.should_not be_nil
   end
 
   it "should be valid" do
