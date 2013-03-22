a9fc92044722348293b7fee608305ac19109ead2
spirula-sga <salimata.gaye@spirula.fr>
2013-03-22 17:33:08 +0100

    Installing Gem with migration (tables): refs #402

--- app/models/pbs_project_element.rb
+++ app/models/pbs_project_element.rb
@@ -30,9 +30,6 @@ class PbsProjectElement < ActiveRecord::Base
 
   has_and_belongs_to_many :module_projects
 
-  #for Estimation purpose
-  ##has_many :ej_estimation_values
-
   validates_presence_of :name
 
   #Sunspot needs
