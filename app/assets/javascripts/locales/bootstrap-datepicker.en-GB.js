8163e9370e1bd207be54de84c811e82429706ecf
olivierpinette <olivier.pinette@spirula.fr>
2013-04-11 12:12:26 +0200

    Fixing DatePicker (refs: #440)

--- app/assets/javascripts/locales/bootstrap-datepicker.en-GB.js
+++ app/assets/javascripts/locales/bootstrap-datepicker.en-GB.js
@@ -8,7 +8,7 @@
     daysMin: ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"],
     months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
     monthsShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-    today: "Today"
+    today: "Today",
         weekStart: 1,
         format: "dd/mm/yyyy"
     };
