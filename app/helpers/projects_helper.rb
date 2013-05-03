65894ed94c4591f6cf3f5028b3458e98addecba0
spirula-sga <salimata.gaye@spirula.fr>
2013-05-03 15:07:08 +0200

    simplify the "edit project module" page: refs #467

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -268,7 +268,6 @@ module ProjectsHelper
                     else
                       readonly_option = wbs_project_elt.has_children? ? true : false
                       nullity_condition = (level_estimation_values.nil? or level_estimation_values[pbs_project_element.id].nil? or level_estimation_values[pbs_project_element.id][wbs_project_elt.id].nil?)
-                      last_value = nullity_condition ? 0 : level_estimation_values[pbs_project_element.id][wbs_project_elt.id]
                       res << "#{ text_field_tag "[#{level}][#{est_val.pe_attribute.alias.to_sym}][#{module_project.id.to_s}][#{wbs_project_elt.id.to_s}]", nullity_condition ? nil : level_estimation_values[pbs_project_element.id][wbs_project_elt.id], :readonly => readonly_option, :class => 'input-mini' }"
                     end
 
