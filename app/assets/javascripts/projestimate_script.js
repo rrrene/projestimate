2ce33d18e71e1762bdced5a657555d7cba03966d
ssebbar <sabrina.sebbar@spirula.fr>
2013-06-03 14:50:26 +0200

    Enhance pages with tabs refs  #489

--- app/assets/javascripts/projestimate_script.js
+++ app/assets/javascripts/projestimate_script.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
 
+    $('.tabs').tabs({
+        select: function(event, ui) {
+            var index_tab = ui.index + 1;
+            $(".current_tab").val("tabs-" + index_tab);
+        }
+    });
+
     $('.attribute_tooltip').tooltip({'html' : true });
 
     $("#run_estimation").bind('click', function() {
