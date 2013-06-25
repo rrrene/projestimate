3a8686b31973d2ef193b948ba9d9cda0fd88cd1e
nicolas <renard760@gmail.com>
2013-06-25 14:34:32 +0200

    refs #508 - graphical improvement

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -189,7 +189,7 @@ class UsersController < ApplicationController
 
   def find_use_user
     @user = User.find(params[:user_id])
-    @relations = @user.projects
+    @related_projects = @user.projects
   end
 
   def about
