c4b62f657381242b9062c08aaa2a4d0100ffe820
nicolas <renard760@gmail.com>
2013-07-05 16:28:53 +0200

    refs #499

--- app/assets/javascripts/projestimate_script.js
+++ app/assets/javascripts/projestimate_script.js
@@ -276,6 +276,100 @@ $(document).ready(function() {
 
     $('html, body').animate({ scrollTop: 0 });
 
+    $.ui.plugin.add("resizable", "alsoResizeReverse", {
+
+        start: function(event, ui) {
+
+            var self = $(this).data("resizable"), o = self.options;
+
+            var _store = function(exp) {
+                $(exp).each(function() {
+                    $(this).data("resizable-alsoresize-reverse", {
+                        width: parseInt($(this).width(), 10), height: parseInt($(this).height(), 10),
+                        left: parseInt($(this).css('left'), 10), top: parseInt($(this).css('top'), 10)
+                    });
+                });
+            };
+
+            if (typeof(o.alsoResizeReverse) == 'object' && !o.alsoResizeReverse.parentNode) {
+                if (o.alsoResizeReverse.length) { o.alsoResize = o.alsoResizeReverse[0];    _store(o.alsoResizeReverse); }
+                else { $.each(o.alsoResizeReverse, function(exp, c) { _store(exp); }); }
+            }else{
+                _store(o.alsoResizeReverse);
+            }
+        },
+
+        resize: function(event, ui){
+            var self = $(this).data("resizable"), o = self.options, os = self.originalSize, op = self.originalPosition;
+
+            var delta = {
+                    height: (self.size.height - os.height) || 0, width: (self.size.width - os.width) || 0,
+                    top: (self.position.top - op.top) || 0, left: (self.position.left - op.left) || 0
+                },
+
+                _alsoResizeReverse = function(exp, c) {
+                    $(exp).each(function() {
+                        var el = $(this), start = $(this).data("resizable-alsoresize-reverse"), style = {}, css = c && c.length ? c : ['width', 'height', 'top', 'left'];
+
+                        $.each(css || ['width', 'height', 'top', 'left'], function(i, prop) {
+                            var sum = (start[prop]||0) - (delta[prop]||0); // subtracting instead of adding
+                            if (sum && sum >= 0)
+                                style[prop] = sum || null;
+                        });
+
+                        //Opera fixing relative position
+                        if (/relative/.test(el.css('position')) && $.browser.opera) {
+                            self._revertToRelativePosition = true;
+                            el.css({ position: 'absolute', top: 'auto', left: 'auto' });
+                        }
+
+                        el.css(style);
+                    });
+                };
+
+            if (typeof(o.alsoResizeReverse) == 'object' && !o.alsoResizeReverse.nodeType) {
+                $.each(o.alsoResizeReverse, function(exp, c) { _alsoResizeReverse(exp, c); });
+            }else{
+                _alsoResizeReverse(o.alsoResizeReverse);
+            }
+        },
+
+        stop: function(event, ui){
+            var self = $(this).data("resizable");
+
+            //Opera fixing relative position
+            if (self._revertToRelativePosition && $.browser.opera) {
+                self._revertToRelativePosition = false;
+                el.css({ position: 'relative' });
+            }
+
+            $(this).removeData("resizable-alsoresize-reverse");
+        }
+    });
+
+
+    $(".input").resizable({
+        alsoResizeReverse: ".output",
+        handles: "e, w"
+    });
+
+    $(".output").resizable({
+        alsoResizeReverse: ".input",
+        handles: "e, w"
+    });
+
+    $(".estimation_plan").resizable({
+        alsoResizeReverse: ".pbs",
+        handles: "e, w"
+    });
+
+    $(".pbs").resizable({
+        alsoResizeReverse: ".estimation_plan"
+    });
+
+    $(".ui-resizable-handle").css("background-image", 'none');
+
+
 });
 
 // ################################# Other methods #################################
