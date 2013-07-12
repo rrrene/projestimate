b61bb59b37170ec950e7c638945c843e8a599657
nicolas <renard760@gmail.com>
2013-07-12 14:40:26 +0200

    refs #488

--- lib/data_validation_helper.rb
+++ lib/data_validation_helper.rb
@@ -39,10 +39,12 @@ module DataValidationHelper
 
       ##get all has many relations et for each...
       @record.transaction do
-        @record.class.reflect_on_all_associations(:has_many).map{|i| i.name }.each do |associated_class_name|
-          @record.parent_reference.send(associated_class_name).each do |obj|
-            obj.send("#{@record.class.to_s.underscore}_id=", @record.id)
-            obj.save
+        unless @record.class == PeAttribute
+          @record.class.reflect_on_all_associations(:has_many).map{|i| i.name }.each do |associated_class_name|
+            @record.parent_reference.send(associated_class_name).each do |obj|
+              obj.send("#{@record.class.to_s.underscore}_id=", @record.id)
+              obj.save
+            end
           end
         end
       end
