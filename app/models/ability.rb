15a34afddab12e58a485642765fcf0b588a33698
nicolas <renard760@gmail.com>
2013-07-23 14:25:03 +0200

    refs #272

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -26,7 +26,7 @@ class Ability
   def initialize(user)
 #    Uncomment in order to authorize everybody to manage all the app
 
-    can :manage, :all
+    #can :manage, :all
     cannot :update, :all, :record_status => {:name => "Retired"}
 
     #Load user groups permissions
@@ -43,13 +43,13 @@ class Ability
       end
 
       #Specfic project security loading
-      #prj_scrt = ProjectSecurity.find_by_user_id(user.id)
-      #unless prj_scrt.nil?
-      #  specific_permissions_array = prj_scrt.project_security_level.permissions.map{|i| [i.name, i.object_associated.constantize] }
-      #  for perm in specific_permissions_array
-      #    can perm[0].to_sym, perm[1]
-      #  end
-      #end
+      prj_scrt = ProjectSecurity.find_by_user_id(user.id)
+      unless prj_scrt.nil?
+        specific_permissions_array = prj_scrt.project_security_level.permissions.map{|i| [i.name, i.object_associated.constantize] }
+        for perm in specific_permissions_array
+          can perm[0].to_sym, perm[1]
+        end
+      end
 
       user.group_for_project_securities.each do |grp|
         prj_scrt = ProjectSecurity.find_by_group_id(grp.id)
