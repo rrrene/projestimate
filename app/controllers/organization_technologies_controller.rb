68e3d881dea0275894ac8cad651b88d9ed5e442a
Sabrina <sabrina.sebbar@spirula.fr>
2013-08-07 11:28:59 +0200

    Organization Technology refs #585

--- app/controllers/organization_technologies_controller.rb
+++ app/controllers/organization_technologies_controller.rb
@@ -24,22 +24,32 @@ class OrganizationTechnologiesController < ApplicationController
     authorize! :create_edit_organizations, Organization
 
     @organization_technology = OrganizationTechnology.new(params[:organization_technology])
-    @organization_technology.save
-    redirect_to edit_organization_path(params[:organization_technology][:organization_id])
+    if @organization_technology.save
+      flash[:notice] = I18n.t (:notice_organization_technology_successful_created)
+      redirect_to redirect_apply(nil, new_organization_technology_path(params[:organization_technology]),edit_organization_path(params[:organization_technology][:organization_id], :anchor=>'tabs-4'))
+    else
+      render action: 'new'
+      end
   end
 
   def update
     authorize! :create_edit_organizations, Organization
 
     @organization_technology = OrganizationTechnology.find(params[:id])
-    @organization_technology.update_attributes(params[:organization_technology])
-    redirect_to edit_organization_path(@organization_technology.organization_id)
+    if @organization_technology.update_attributes(params[:organization_technology])
+      flash[:notice] = I18n.t (:notice_organization_technology_successful_updated)
+      redirect_to redirect_apply(edit_organization_technology_path(params[:organization_technology]),nil,edit_organization_path(params[:organization_technology][:organization_id], :anchor=>'tabs-4'))
+    else
+      render action: 'edit'
+    end
   end
 
   def destroy
     @organization_technology = OrganizationTechnology.find(params[:id])
     organization_id = @organization_technology.organization_id
     @organization_technology.delete
-    redirect_to edit_organization_path(organization_id)
+    respond_to do |format|
+      format.html { redirect_to redirect(edit_organization_path(organization_id, :anchor=>'tabs-4')), notice: "#{I18n.t (:notice_organization_technology_successful_deleted)}"}
+    end
   end
 end
