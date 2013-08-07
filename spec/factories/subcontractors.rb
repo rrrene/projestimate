7c7122707ffaf57817347579b74b8834d7ae3995
spirula-sga <salimata.gaye@spirula.fr>
2013-08-07 14:32:46 +0200

    Organization subcontractor: refs  #573

--- spec/factories/subcontractors.rb
+++ spec/factories/subcontractors.rb
@@ -1,10 +1,10 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :subcontractor do
-    organization_id 1
-    name "MyString"
-    subcontractor_alias "MyString"
-    description "MyText"
+  factory :subcontractor do |sub|
+    sub.organization_id 1
+    sub.name "MyString"
+    sub.alias "MyString"
+    sub.description "MyText"
   end
 end
