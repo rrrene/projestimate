aa8dfbc957d2947ff45a23420a126a2d846d2efb
nicolas <renard760@gmail.com>
2013-08-27 17:12:26 +0200

    refs #589

---
+++ app/models/abacus_organization.rb
@@ -0,0 +1,6 @@
+class AbacusOrganization < ActiveRecord::Base
+  attr_accessible :organization_technology_id, :organization_uow_complexity_id, :unit_or_work_id, :value
+
+  belongs_to :unit_of_work
+  belongs_to :organization_uow_complexity
+end
