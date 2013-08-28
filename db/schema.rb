eaf88b1504fc3d2fa9352df976eefb818bb2dc75
nicolas <renard760@gmail.com>
2013-08-28 16:04:36 +0200

    Re-Allow to create inactive user.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130823134708) do
+ActiveRecord::Schema.define(:version => 20130828132737) do
 
   create_table "abacus_organizations", :force => true do |t|
     t.float    "value"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20130823134708) do
     t.integer  "organization_technology_id"
     t.datetime "created_at",                     :null => false
     t.datetime "updated_at",                     :null => false
+    t.integer  "organization_id"
   end
 
   create_table "acquisition_categories", :force => true do |t|
