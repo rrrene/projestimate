0a1cc8480111e92edc4b59e5973e86fdde867355
Sabrina <sabrina.sebbar@spirula.fr>
2013-09-04 15:46:39 +0200

    Increase test code coverage  refs #554

--- spec/unit/project_spec.rb
+++ spec/unit/project_spec.rb
@@ -4,33 +4,29 @@ describe Project do
 
   before :each do
     @project = FactoryGirl.create(:project) #, :title => "projet11", :alias => "P11")
-    @user = FactoryGirl.build(:user)
-    @user1 = FactoryGirl.build(:user)
+    @user = FactoryGirl.create(:user)
+
     #@user1 = User.new(:last_name => 'Projestimate', :first_name => 'Administrator', :login_name => 'admin1', :email => 'youremail1@yourcompany.net', :user_status => 'active', :auth_type => AuthMethod.first.id, :password => 'test', :password_confirmation => 'test')
     #@project1 = Project.new(:title => 'Projet1', :description => 'projet numero 1', :alias => 'P1', :state => 'preliminary')
     @project1 = FactoryGirl.build(:project)
-    @project_security_1 = ProjectSecurity.new(:project_id => @project1.id, :user_id => @user1.id)
-    @project_security = ProjectSecurity.new(:project_id => @project.id, :user_id => @user1.id)
-  end
+    @project_security_1 = ProjectSecurity.new(:project_id => @project1.id, :user_id => @user.id)
+    @project_security = ProjectSecurity.new(:project_id => @project.id, :user_id => @user.id)
 
-  before :all do
-    @user2 = FactoryGirl.build(:user)
-    #@user2 = User.new(:last_name => 'Projestimate', :first_name => 'Administrator', :login_name => 'admin2', :email => 'youremail2@yourcompany.net', :user_status => 'active', :auth_type => 6, :password => 'test', :password_confirmation => 'test')
     @project2 = Project.new(:title => 'Projet1', :description => 'projet numero 1', :alias => 'P1', :state => 'preliminary')
-    @project2_security = ProjectSecurity.new(:project_id => @project2.id, :user_id => @user2.id)
+    @project2_security = ProjectSecurity.new(:project_id => @project2.id, :user_id => @user.id)
   end
 
   it 'should be a valid project' do
     @project.should be_valid
   end
 
-  specify "should be a valid user (user1)" do
-    @user1.should be_valid
+  it  "should be a valid user (user1)" do
+    @user.should be_valid
   end
 
-  specify "should be a valid user (user2)" do
-    @user2.should be_valid
-  end
+  #specify "should be a valid user (user2)" do
+  #  @user2.should be_valid
+  #end
 
   #validation
   it "should not be valid without title"  do
@@ -165,10 +161,10 @@ describe Project do
     Project::encoding.should be_an_instance_of(Array)
   end
 
-  #it " should duplicate project" do
-  #  @project = FactoryGirl.create(:project)
-  #  @project4=@project.amoeba_dup
-  #end
+  it " should duplicate project" do
+    @project = FactoryGirl.create(:project)
+    @project4=@project.amoeba_dup
+  end
 
   after :each do
     #clean up
