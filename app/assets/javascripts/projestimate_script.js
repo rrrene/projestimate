8585c1dbd2c22c0df1b2dbec8d76c97eeb8ae914
Sabrina <sabrina.sebbar@spirula.fr>
2013-09-10 14:29:13 +0200

    Add a Send feedback form  refs #513

--- app/assets/javascripts/projestimate_script.js
+++ app/assets/javascripts/projestimate_script.js
@@ -1913,28 +1913,30 @@ var warnLeavingUnsavedMessage;
 function warn_me(message){
     warnLeavingUnsavedMessage = message;
     $('.simple_form').submit(function(){
-        $('textarea').not("table.tablesorterPager textarea").removeData('changed');
-        $('input').not("table.tablesorterPager input").removeData('changed');
+        $('textarea').not("table.tablesorterPager textarea").not("#send_feedback_description").removeData('changed');
+        $('input').not("table.tablesorterPager input").not("#send_feedback_user_name").removeData('changed');
     });
-    $('textarea').not("table.tablesorterPager textarea").change(function(){
+    $('textarea').not("table.tablesorterPager textarea").not("#send_feedback_description").change(function(){
         $(this).data('changed', 'changed');
     });
-    $('input').not("table.tablesorterPager input").change(function(){
+    $('input').not("table.tablesorterPager input").not("#send_feedback_user_name").change(function(){
         $(this).data('changed', 'changed');
     });
+
     window.onbeforeunload = function(){
         var warn = false;
-        $('textarea').not("table.tablesorterPager textarea").blur().each(function(){
-            if ($(this).data('changed')) {
-                warn = true;
-            }
-        });
-        $('input').not("table.tablesorterPager input").blur().each(function(){
+        $('textarea').not("table.tablesorterPager textarea").not("#send_feedback_description").blur().each(function(){
             if ($(this).data('changed')) {
                 warn = true;
             }
         });
 
+
+        $('input').not("table.tablesorterPager input").not("#send_feedback_user_name").blur().each(function(){
+            if ($(this).data('changed')) {
+                warn = true;
+            }
+        });
         if (warn) {return warnLeavingUnsavedMessage;}
     }
 }
