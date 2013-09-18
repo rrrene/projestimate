c6bb046b85cbd88e59d0466d2d4c49375849190b
nicolas <renard760@gmail.com>
2013-09-18 15:40:31 +0200

    refs #581

--- app/controllers/permissions_controller.rb
+++ app/controllers/permissions_controller.rb
@@ -38,9 +38,14 @@ class PermissionsController < ApplicationController
 
   def globals_permissions
     set_page_title 'Globals Permissions'
-    @permissions = Permission.order('object_associated').defined.select{|i| !i.is_permission_project }
-    @permissions_classes = @permissions.map(&:object_associated).uniq
 
+    @global_permissions = Permission.order('object_associated').defined.select{|i| !i.is_permission_project }
+    @permission_projects = Permission.order('object_associated').defined.select{|i| i.is_permission_project }
+
+    @permissions_classes_globals = @global_permissions.map(&:category).uniq
+    @permissions_classes_projects = @permission_projects.map(&:category).uniq
+
+    @project_security_levels = ProjectSecurityLevel.defined
     @groups = Group.defined_or_local
 
     respond_to do |format|
